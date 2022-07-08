701a2d2322e9426b6e0841d2e076ab35f8dea4e8
gVguy <88876408+gVguy@users.noreply.github.com>
2022-07-08 12:08:11 +0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from 'rollup-plugin-typescript2'
 import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 import resolve from '@rollup/plugin-node-resolve'
